51ecf4b61ff57cc201480851192f1282b842ccf6
BLUE-BLUR\Lerenwe <johan.nagy.contact@gmail.com>
2019-05-09 17:03:53 +0200

    Added collision system

--- Assets/CustomCharaController.cs
+++ Assets/CustomCharaController.cs
@@ -10,14 +10,23 @@ public class CustomCharaController : MonoBehaviour
 
     //Directions
     float xDirection = 0;
-    float yDirection = 0;
     [SerializeField] float speed = 1f;
+
+    //Physic
+    public float accelX;
+    public float decelX;
+    public float gravity;
+    public float groundVelY;
+
+    private Vector3 velocity;
+    private Character character;
 
     public bool flipped = false;
 
     // Start is called before the first frame update
     void Start()
     {
+        character = this.GetComponent<Character>();
         animator = this.GetComponent<Animator>();
         renderer = this.GetComponent<SpriteRenderer>();
     }
@@ -25,30 +34,40 @@ public class CustomCharaController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("H Axis Input = " + Input.GetAxis("Horizontal"));
+        //Debug.Log("H Axis Input = " + Input.GetAxis("Horizontal"));
 
-        if (Input.GetAxisRaw("Horizontal") != 0)
+        xDirection = Input.GetAxisRaw("Horizontal");
+        Debug.Log("xDirection = " + xDirection);
+        //Move!!
+        if (Mathf.Abs(xDirection) > 0f)
         {
+            velocity.x = Mathf.MoveTowards(velocity.x, speed * xDirection, Time.deltaTime * accelX);
+
             //Walking animator
             animator.SetBool("isWalking", true);
-
-            //GetDirection
-            xDirection = Input.GetAxisRaw("Horizontal") * speed;
         }
         else
         {
+            velocity.x = Mathf.MoveTowards(velocity.x, 0f, Time.deltaTime * decelX);
+
             animator.SetBool("isWalking", false);
-            xDirection = 0;
         }
 
-        //Move!!
-        Vector3 direction = new Vector3(xDirection, yDirection, 0);
-        transform.position += direction * speed * Time.deltaTime;
+        //Just well... Applying gravity, y'know.
+        velocity.y += gravity * .5f * Time.deltaTime;
+
+        character.MoveX(velocity.x * Time.deltaTime, Character.Slide.Perpendicular);
+        Debug.Log("Velocity.x = " + velocity.x);
+
+        if (!character.MoveY(velocity.y * Time.deltaTime))
+            velocity.y = groundVelY;
+        else
+            velocity.y += gravity * .5f * Time.deltaTime;
 
         //Flip sprite
-        if (direction.x < -.01f)
+        if (xDirection < -.01f)
             renderer.flipX = true;
-        else if (direction.x > .01f)
+        else if (xDirection > .01f)
             renderer.flipX = false;
 
     }
